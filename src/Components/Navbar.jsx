f881a1e072afe826ca7efcf1dd2de2bd0364b7b5
Hammad Khan <hk188540@gmail.com>
2024-08-28 17:20:24 +0500

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   const {amount} = useGlobalContext()
   return (
-    <nav className='flex justify-around items-center h-20 bg-blue-500 text-white'>
+    <nav className='flex justify-around items-center h-20 bg-blue-500 text-white sticky top-0 right-0 left-0'>
       <h1 className='text-2xl sm:text-3xl tracking-wider font-semibold'>UseReducer</h1>
       <div className='relative'>
         <FaCartPlus className='text-2xl sm:text-3xl'/>
